9fd70603c0db3f32a3c00c3a9adadd4fa4dcdf3d
rafael <rafaelcnunes@icloud.com>
2023-02-05 03:49:49 -0300

    added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Codebook')
+    .setDescription('The Codebook API description')
+    .setVersion('1.0')
+    .addTag('codebook')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
